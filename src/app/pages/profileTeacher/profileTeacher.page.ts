17f539797cc3977ffb7b9882eae791480374c5ba
dariasv1108 <dariasv01@gmail.com>
2020-01-28 17:56:10 +0100

    userint

--- src/app/pages/profileTeacher/profileTeacher.page.ts
+++ src/app/pages/profileTeacher/profileTeacher.page.ts
@@ -1,7 +1,12 @@
-import { Teacher } from './../../core/model/teacher';
-import { Component, OnInit } from '@angular/core';
-import { SingUpServiceService } from 'src/app/servicios/singUp/sing-up-service.service';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+
+
+import { Teacher } from './../../core/model/teacher';
+import { SingUpServiceService } from 'src/app/servicios/singUp/sing-up-service.service';
+declare var google;
 @Component({
   selector: 'app-profileTeacher',
   templateUrl: './profileTeacher.page.html',
@@ -9,16 +14,72 @@ import { SingUpServiceService } from 'src/app/servicios/singUp/sing-up-service.s
 })
 export class ProfileTeacherPage implements OnInit {
 
-  private teacher : Teacher = new Teacher();
+  @ViewChild('map', { static: false }) mapElement: ElementRef;
+  map: any;
+  address: string;
 
+  private teacher: Teacher = new Teacher();
 
-  constructor(private signUp: SingUpServiceService) {
+  constructor(private signUp: SingUpServiceService, private geolocation: Geolocation,
+    private nativeGeocoder: NativeGeocoder) {
   }
 
   ngOnInit() {
+    this.loadMap();
   }
 
   save() {
     this.signUp.addTeacher(this.teacher);
+  }
+
+  loadMap() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.getAddressFromCoords(resp.coords.latitude, resp.coords.longitude);
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      this.map.addListener('tilesloaded', () => {
+        console.log('accuracy', this.map);
+        this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng())
+      });
+
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  getAddressFromCoords(lattitude, longitude) {
+    console.log("getAddressFromCoords " + lattitude + " " + longitude);
+    let options: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5
+    };
+
+    this.nativeGeocoder.reverseGeocode(lattitude, longitude, options)
+      .then((result: NativeGeocoderResult[]) => {
+        this.address = "";
+        let responseAddress = [];
+        for (let [key, value] of Object.entries(result[0])) {
+          if (value.length > 0)
+            responseAddress.push(value);
+
+        }
+        responseAddress.reverse();
+        for (let value of responseAddress) {
+          this.address += value + ", ";
+        }
+        this.address = this.address.slice(0, -2);
+      })
+      .catch((error: any) => {
+        this.address = "Address Not Available!";
+      });
+
   }
 }
